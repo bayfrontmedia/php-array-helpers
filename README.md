cd9133344d708240aac0062ea193504f93bf0fab
John Robinson <john@bayfrontmedia.com>
2020-07-27 14:17:08 -0400

    Initial release

---
+++ README.md
@@ -0,0 +1,478 @@
+## PHP array helpers
+
+PHP helper class to provide useful array functions.
+
+- [License](#license)
+- [Author](#author)
+- [Requirements](#requirements)
+- [Installation](#installation)
+- [Usage](#usage)
+
+## License
+
+This project is open source and available under the [MIT License](https://github.com/bayfrontmedia/php-array-helpers/blob/master/LICENSE).
+
+## Author
+
+John Robinson, [Bayfront Media](https://www.bayfrontmedia.com)
+
+## Requirements
+
+* PHP > 7.1.0
+
+## Installation
+
+```
+composer require bayfrontmedia/php-array-helpers
+```
+
+## Usage
+
+- [dot](#dot)
+- [undot](#undot)
+- [set](#set)
+- [has](#has)
+- [get](#get)
+- [pluck](#pluck)
+- [forget](#forget)
+- [except](#except)
+- [only](#only)
+- [missing](#missing)
+- [isMissing](#ismissing)
+- [multisort](#multisort)
+- [query](#query)
+
+<hr />
+
+### dot
+
+**Description:**
+
+Converts a multidimensional array to a single depth "dot" notation array, optionally prepending a string to each array key.
+
+The key values will never be an array, even if empty. Empty arrays will be dropped.
+
+**Parameters:**
+
+- `$array` (array): Original array
+- `$prepend = ''` (string): String to prepend
+
+**Returns:**
+
+- (array)
+
+**Example:**
+
+```
+use Bayfront\ArrayHelpers\Arr;
+
+$array = [
+    'name' => [
+        'first_name' => 'John',
+        'last_name' => 'Doe'
+    ],
+    'hobbies' => [ // This empty array will be dropped
+
+    ]
+];
+
+$dot = Arr::dot($array);
+```
+
+<hr />
+
+### undot
+
+**Description:**
+
+Converts array in "dot" notation to a standard multidimensional array.
+
+**Parameters:**
+
+- `$array` (array): Array in "dot" notation
+
+**Returns:**
+
+- (array)
+
+**Example:**
+
+```
+use Bayfront\ArrayHelpers\Arr;
+
+$array = [
+    'name.first_name' => 'John',
+    'name.last_name' => 'Doe'
+];
+
+$undot = Arr::undot($array);
+```
+
+<hr />
+
+### set
+
+**Description:**
+
+Set an array item to a given value using "dot" notation.
+
+**Parameters:**
+
+- `$array` (array): Original array
+- `$key` (string): Key to set in "dot" notation
+- `$value` (mixed): Value of key
+
+**Returns:**
+
+- (array)
+
+**Example:**
+
+```
+use Bayfront\ArrayHelpers\Arr;
+
+$array = [
+    'name' => [
+        'first_name' => 'John',
+        'last_name' => 'Doe'
+    ],
+];
+
+$array = Arr::set($array, 'name.middle_name', 'Middle');
+```
+
+<hr />
+
+### has
+
+**Description:**
+
+Checks if array key exists and not null using "dot" notation.
+
+**Parameters:**
+
+- `$array` (array): Original array
+- `$key` (string): Key to check in "dot" notation
+
+**Returns:**
+
+- (bool)
+
+**Example:**
+
+```
+use Bayfront\ArrayHelpers\Arr;
+
+$array = [
+    'name' => [
+        'first_name' => 'John',
+        'last_name' => 'Doe'
+    ],
+];
+
+if (Arr::has($array, 'name.first_name')) {
+    // Do something
+}
+```
+
+<hr />
+
+### get
+
+**Description:**
+
+Get an item from an array using "dot" notation, returning an optional default value if not found.
+
+**Parameters:**
+
+- `$array` (array): Original array
+- `$key` (string): Key to return in "dot" notation
+- `$default = NULL` (mixed): Default value to return
+
+**Returns:**
+
+- (mixed)
+
+**Example:**
+
+```
+use Bayfront\ArrayHelpers\Arr;
+
+$array = [
+    'name' => [
+        'first_name' => 'John',
+        'last_name' => 'Doe'
+    ],
+];
+
+echo Arr::get($array, 'name.first_name');
+```
+
+<hr />
+
+### pluck
+
+**Description:**
+
+Returns an array of values for a given key from an array using "dot" notation.
+
+**Parameters:**
+
+- `$array` (array): Original array
+- `$value` (string): Value to return in "dot" notation
+- `$key = NULL` (string): Optionally how to key the returned array in "dot" notation
+
+**Returns:**
+
+- (array)
+
+**Example:**
+
+```
+use Bayfront\ArrayHelpers\Arr;
+
+$array = [
+    [
+        'user_id' => 110,
+        'username' => 'John',
+        'active' => true
+    ],
+    [
+        'user_id' => 111,
+        'username' => 'Jane',
+        'active' => true
+    ]
+];
+
+$array = Arr::pluck($array, 'username', 'user_id');
+```
+
+<hr />
+
+### forget
+
+**Description:**
+
+Remove a single key, or an array of keys from a given array using "dot" notation.
+
+**Parameters:**
+
+- `$array` (array): Original array
+- `$keys` (string|array): Key(s) to forget in "dot" notation
+
+**Returns:**
+
+- (void)
+
+**Example:**
+
+```
+use Bayfront\ArrayHelpers\Arr;
+
+$array = [
+    'name' => [
+        'first_name' => 'John',
+        'last_name' => 'Doe'
+    ],
+];
+
+Arr::forget($array, 'name.last_name');
+```
+
+<hr />
+
+### except
+
+**Description:**
+
+Returns the original array except given key(s).
+
+**Parameters:**
+
+- `$array` (array): Original array
+- `$keys` (string|array): Key(s) to remove
+
+**Returns:**
+
+- (array)
+
+**Example:**
+
+```
+use Bayfront\ArrayHelpers\Arr;
+
+$array = [
+    'user_id' => 110,
+    'username' => 'John',
+    'active' => true
+];
+
+$array = Arr::except($array, 'active');
+```
+
+<hr />
+
+### only
+
+**Description:**
+
+Returns only desired key(s) from an array.
+
+**Parameters:**
+
+- `$array` (array): Original array
+- `$keys` (string|array): Key(s) to return
+
+**Returns:**
+
+- (array)
+
+**Example:**
+
+```
+use Bayfront\ArrayHelpers\Arr;
+
+$array = [
+    'user_id' => 110,
+    'username' => 'John',
+    'active' => true
+];
+
+$array = Arr::only($array, 'username');
+```
+
+<hr />
+
+### missing
+
+**Description:**
+
+Returns array of missing keys from the original array, or an empty array if none are missing.
+
+**Parameters:**
+
+- `$array` (array): Original array
+- `$keys` (array): Key(s) to check
+
+**Returns:**
+
+- (array)
+
+**Example:**
+
+```
+use Bayfront\ArrayHelpers\Arr;
+
+$array = [
+    'user_id' => 110,
+    'username' => 'John',
+    'active' => true
+];
+
+$missing = Arr::missing($array, [
+    'active',
+    'last_login'
+]);
+```
+
+<hr />
+
+### isMissing
+
+**Description:**
+
+Checks if keys are missing from the original array.
+
+**Parameters:**
+
+- `$array` (array): Original array
+- `$keys` (array): Key(s) to check
+
+**Returns:**
+
+- (bool)
+
+**Example:**
+
+```
+use Bayfront\ArrayHelpers\Arr;
+
+$array = [
+    'user_id' => 110,
+    'username' => 'John',
+    'active' => true
+];
+
+if (Arr::isMissing($array, [
+    'active',
+    'last_login'
+])) {
+    // Do something
+}
+```
+
+<hr />
+
+### multisort
+
+**Description:**
+
+Sort a multidimensional array by a given key in ascending (optionally, descending) order.
+
+**Parameters:**
+
+- `$array` (array): Original array
+- `$key` (string): Key name to sort by
+- `$descending = false` (bool): Sort descending
+
+**Returns:**
+
+- (array)
+
+**Example:**
+
+```
+use Bayfront\ArrayHelpers\Arr;
+
+$clients = [
+    [
+        'first_name' => 'John',
+        'last_name' => 'Doe'
+    ],
+    [
+        'first_name' => 'Jane',
+        'last_name' => 'Doe'
+    ]
+];
+
+$sorted = Arr::multisort($clients, 'first_name');
+```
+
+<hr />
+
+### query
+
+**Description:**
+
+Convert array into a query string.
+
+**Parameters:**
+
+- `$array` (array): Original array
+
+**Returns:**
+
+- (string)
+
+**Example:**
+
+```
+use Bayfront\ArrayHelpers\Arr;
+
+$array = [
+    'first_name' => 'Jane',
+    'last_name' => 'Doe'
+];
+
+echo Arr::query($array);
+```
---
+++ src/Arr.php
@@ -0,0 +1,350 @@
+<?php
+/**
+ * Helper class to provide useful array functions
+ *
+ * Heavily inspired by: https://laravel.com/docs/7.x/helpers
+ * See: https://github.com/laravel/framework/blob/5.8/src/Illuminate/Support/Arr.php
+ *
+ * @version     1.1.0
+ * @link        https://github.com/bayfrontmedia/php-array-helpers
+ * @license     MIT https://opensource.org/licenses/MIT
+ * @copyright   2020 Bayfront Media https://www.bayfrontmedia.com
+ * @author      John Robinson <john@bayfrontmedia.com>
+ */
+
+namespace Bayfront\ArrayHelpers;
+
+class Arr
+{
+
+    /**
+     * Converts a multidimensional array to a single depth "dot" notation array,
+     * optionally prepending a string to each array key
+     *
+     * The key values will never be an array, even if empty. Empty arrays will be dropped.
+     *
+     * See also: https://stackoverflow.com/a/10424516
+     *
+     * @param array $array (Original array)
+     * @param string $prepend (String to prepend)
+     *
+     * @return array
+     */
+
+    public static function dot(array $array, string $prepend = ''): array
+    {
+
+        $results = [];
+
+        foreach ($array as $key => $value) {
+
+            if (is_array($value)) {
+
+                $results = array_merge($results, self::dot($value, $prepend . $key . '.'));
+
+            } else {
+
+                $results[$prepend . $key] = $value;
+
+            }
+
+            // Empty arrays are not returned
+
+        }
+
+        return $results;
+
+    }
+
+    /**
+     * Converts array in "dot" notation to a standard multidimensional array
+     *
+     * @param array $array (Array in "dot" notation)
+     *
+     * @return array
+     */
+
+    public static function undot(array $array): array
+    {
+
+        $return = [];
+
+        foreach ($array as $key => $value) {
+
+            self::set($return, $key, $value);
+
+        }
+
+        return $return;
+
+    }
+
+    /**
+     * Set an array item to a given value using "dot" notation
+     *
+     * @param array $array (Original array)
+     * @param string $key (Key to set in "dot" notation)
+     * @param mixed $value (Value of key)
+     *
+     * @return array (Modified array)
+     */
+
+    public static function set(&$array, string $key, $value): array
+    {
+
+        $keys = explode('.', $key);
+
+        while (count($keys) > 1) {
+
+            $key = array_shift($keys);
+
+            /*
+             * If the key doesn't exist at this depth, an empty array is created
+             * to hold the next value, allowing to create the arrays to hold final
+             * values at the correct depth. Then, keep digging into the array.
+             */
+
+            if (!isset($array[$key]) || !is_array($array[$key])) {
+
+                $array[$key] = [];
+
+            }
+
+            $array = &$array[$key];
+
+        }
+
+        $array[array_shift($keys)] = $value;
+
+        return $array;
+
+    }
+
+    /**
+     * Checks if array key exists and not null using "dot" notation
+     *
+     * @param array $array (Original array)
+     * @param string $key (Key to check in "dot" notation)
+     *
+     * @return bool
+     */
+
+    public static function has(array $array, string $key): bool
+    {
+        return NULL !== self::get($array, $key);
+    }
+
+    /**
+     * Get an item from an array using "dot" notation, returning an optional default value if not found
+     *
+     * @param array $array (Original array)
+     * @param string $key (Key to return in "dot" notation)
+     * @param mixed $default (Default value to return)
+     *
+     * @return mixed
+     */
+
+    public static function get(array $array, string $key, $default = NULL)
+    {
+
+        if (is_null($key)) {
+
+            return $array;
+
+        }
+
+        if (isset($array[$key])) {
+
+            return $array[$key];
+
+        }
+
+        foreach (explode('.', $key) as $segment) {
+
+            if (!is_array($array) || !array_key_exists($segment, $array)) {
+
+                return $default;
+
+            }
+
+            $array = $array[$segment];
+
+        }
+
+        return $array;
+
+    }
+
+    /**
+     * Returns an array of values for a given key from an array using "dot" notation
+     *
+     * @param array $array (Original array)
+     * @param string $value (Value to return in "dot" notation)
+     * @param string $key (Optionally how to key the returned array in "dot" notation)
+     *
+     * @return array
+     */
+
+    public static function pluck(array $array, string $value, string $key = NULL): array
+    {
+        $results = [];
+
+        foreach ($array as $item) {
+
+            $itemValue = self::get($item, $value);
+
+            if (is_null($key)) {
+
+                $results[] = $itemValue;
+
+            } else {
+
+                $itemKey = self::get($item, $key);
+
+                $results[$itemKey] = $itemValue;
+
+            }
+
+        }
+
+        return $results;
+    }
+
+    /**
+     * Remove a single key, or an array of keys from a given array using "dot" notation
+     *
+     * @param array $array (Original array)
+     * @param string|array $keys (Key(s) to forget in "dot" notation)
+     *
+     * @return void
+     */
+
+    public static function forget(array &$array, $keys): void
+    {
+
+        $original =& $array;
+
+        foreach ((array)$keys as $key) {
+
+            $parts = explode('.', $key);
+
+            while (count($parts) > 1) {
+
+                $part = array_shift($parts);
+
+                if (isset($array[$part]) && is_array($array[$part])) {
+
+                    $array =& $array[$part];
+
+                }
+
+            }
+
+            unset($array[array_shift($parts)]);
+
+            // Clean up after each iteration
+
+            $array =& $original;
+
+        }
+    }
+
+    /**
+     * Returns the original array except given key(s)
+     *
+     * @param array $array (Original array)
+     * @param string|array $keys (Keys to remove)
+     *
+     * @return array
+     */
+
+    public static function except(array $array, $keys): array
+    {
+        return array_diff_key($array, array_flip((array)$keys));
+    }
+
+    /**
+     * Returns only desired key(s) from an array
+     *
+     * @param array $array (Original array)
+     * @param string|array $keys (Keys to return)
+     *
+     * @return array
+     */
+
+    public static function only(array $array, $keys): array
+    {
+        return array_intersect_key($array, array_flip((array)$keys));
+    }
+
+    /**
+     * Returns array of missing keys from the original array, or an empty array if none are missing
+     *
+     * @param array $array (Original array)
+     * @param string|array $keys (Keys to check)
+     *
+     * @return array
+     */
+
+    public static function missing(array $array, $keys): array
+    {
+        return array_values(array_flip(array_diff_key(array_flip((array)$keys), $array)));
+    }
+
+    /**
+     * Checks if keys are missing from the original array
+     *
+     * @param array $array (Original array)
+     * @param string|array $keys (Keys to check)
+     *
+     * @return bool
+     */
+
+    public static function isMissing(array $array, $keys): bool
+    {
+        return (self::missing($array, $keys)) ? true : false;
+    }
+
+    /**
+     * Sort a multidimensional array by a given key in ascending (optionally, descending) order
+     *
+     * @param array $array (Original array)
+     * @param string $key (Key name to sort by)
+     * @param bool $descending (Sort descending)
+     *
+     * @return array
+     */
+
+    public static function multisort(array $array, string $key, bool $descending = false): array
+    {
+
+        $columns = array_column($array, $key);
+
+        if (false === $descending) {
+
+            array_multisort($columns, SORT_ASC, $array);
+
+        } else {
+
+            array_multisort($columns, SORT_DESC, $array);
+
+        }
+
+        return $array;
+
+    }
+
+    /**
+     * Convert array into a query string
+     *
+     * @param array $array (Original array)
+     *
+     * @return string
+     */
+
+    public static function query(array $array): string
+    {
+        return http_build_query($array, NULL, '&', PHP_QUERY_RFC3986);
+    }
+
+}
